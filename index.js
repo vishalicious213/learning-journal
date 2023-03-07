a66314709f6ddc37a1c281bc15642d465496558c
vishalicious213 <vishalicious213@gmail.com>
2023-03-07 13:36:32 -0800

    added IDs to the array of articles

--- index.js
+++ index.js
@@ -2,24 +2,28 @@ console.log("Learning journal")
 
 const articles = [
     {
+        id: 1,
         date: "January 1, 2023",
         title: "My new journey as a bootcamp student.",
         blurb: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
         img: "article-image-hero.png"
     },
     {
+        id: 2,
         date: "January 15, 2023",
         title: "Blog one",
         blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         img: "article-image-03.png"
     },
     {
+        id: 3,
         date: "February 1, 2023",
         title: "Blog two",
         blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
         img: "article-image-02.png"
     },
     {
+        id: 4,
         date: "February 15, 2023",
         title: "Blog three",
         blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
