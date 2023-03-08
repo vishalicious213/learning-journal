b8611e35e42a46ba5608709adc444c657eb1d898
vishalicious213 <vishalicious213@gmail.com>
2023-03-07 16:20:54 -0800

    renamed images to match new widescreen article layout

--- index.js
+++ index.js
@@ -2,32 +2,53 @@ console.log("Learning journal")
 
 const articles = [
     {
-        id: 1,
-        date: "January 1, 2023",
+        id: 0,
+        date: "December 27, 2022",
         title: "My new journey as a bootcamp student.",
         blurb: "After several months of learning in the Frontend Developer Career Path, I've made the big jump over to the Bootcamp to get expert code reviews of my Solo Projects projects and meet like-minded peers.",
         img: "article-image-hero.png"
     },
     {
-        id: 2,
-        date: "January 15, 2023",
+        id: 1,
+        date: "January 1, 2023",
         title: "Blog one",
         blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        img: "article-image-03.png"
+        img: "article-image-01.png"
+    },
+    {
+        id: 2,
+        date: "January 15, 2023",
+        title: "Blog two",
+        blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        img: "article-image-02.png"
     },
     {
         id: 3,
         date: "February 1, 2023",
-        title: "Blog two",
+        title: "Blog three",
         blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        img: "article-image-02.png"
+        img: "article-image-03.png"
     },
     {
         id: 4,
         date: "February 15, 2023",
         title: "Blog three",
         blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
-        img: "article-image-01.png"
+        img: "article-image-04.png"
+    },
+    {
+        id: 5,
+        date: "March 1, 2023",
+        title: "Blog three",
+        blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        img: "article-image-05.png"
+    },
+    {
+        id: 6,
+        date: "March 15, 2023",
+        title: "Blog three",
+        blurb: "I'm excited to start a new learning journey as a Scrimba Bootcamp student! After several months of learning in the Frontend Developer Career Path.",
+        img: "article-image-06.png"
     },
 ]
 
@@ -47,7 +68,7 @@ function renderArticles() {
     `
 
     articles.forEach(function(article) {
-        if (article.id > 1) {
+        if (article.id > 0) {
             articlesSection.innerHTML += `
                 <div class="article">
                     <div class="article-img">
