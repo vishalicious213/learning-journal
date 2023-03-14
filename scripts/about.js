ed26f9e9ba92144716ec032324a1bcab0e9d1c15
vishalicious213 <vishalicious213@gmail.com>
2023-03-13 19:49:54 -0700

    about: rendering articles

---
+++ scripts/about.js
@@ -0,0 +1,19 @@
+import { articles } from "./data.js"
+const articlesSection = document.getElementById("articles")
+
+articles.forEach(function(article) {
+    if (article.id > 0) {
+        articlesSection.innerHTML += `
+            <div class="article">
+                <a href="/posts/${article.link}">
+                    <div class="article-img" style="background-image: url(/img/${article.img});"></div>
+                </a>
+                <div class="article-date">${article.date}</div>
+                <a class="article-title" href="/posts/${article.link}">
+                    <div>${article.title}</div>
+                </a>
+                <div class="article-blurb">${article.blurb}</div>
+            </div>
+        `
+    }
+})
